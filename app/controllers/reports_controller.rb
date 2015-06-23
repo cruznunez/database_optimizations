781c4dc642d9b6a59403bd49f0c9d1b651802ae0
Cruz <nunez.a.cruz@gmail.com>
2015-06-23 10:11:54 -0400

    w8d1 hw

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,6 @@ class ReportsController < ApplicationController
   def all_data
     @assembly = Assembly.find_by_name(params[:name])
 
-    #courtesy of RedMeeple
     @hits = Hit.where( subject_id: Gene.where( sequence_id: Sequence.where(
         assembly_id: Assembly.where( name: params[:name] )))).order( :percent_similarity )
   end
