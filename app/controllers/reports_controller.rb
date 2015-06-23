1b119127cac544c81a45229c302abec11f127931
Cruz <nunez.a.cruz@gmail.com>
2015-06-23 13:46:39 -0400

    Change root to assemblyname/email form. Add confirmation page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,4 +5,13 @@ class ReportsController < ApplicationController
     @hits = Hit.where( subject_id: Gene.where( sequence_id: Sequence.where(
         assembly_id: Assembly.where( name: params[:name] )))).order( :percent_similarity )
   end
+
+  def email
+    @choices = Assembly.all.map {|a| a.name}
+  end
+
+  def confirmation
+    @email = params[:email]
+    @name = params[:name]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'reports/all_data'
+  post 'confirmation' => 'reports#confirmation'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'reports#all_data'
+  root 'reports#email'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
